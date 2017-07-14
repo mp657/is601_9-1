a0f8678ec5b63d55aaa03b8a6084d3d95ad63866
monika pattanayak <mp657@njit.edu>
2017-07-14 18:21:05 -0400

    validate name

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-i<?php
+<?php
 //set default values
 $name = '';
 $email = '';
@@ -14,27 +14,10 @@ switch ($action) {
         $email = filter_input(INPUT_POST, 'email');
         $phone = filter_input(INPUT_POST, 'phone');
 
-        /*************************************************
-         * validate and process the name
-         ************************************************/
-        // 1. make sure the user enters a name
-        // 2. display the name with only the first letter capitalized
-
-        /*************************************************
-         * validate and process the email address
-         ************************************************/
-        // 1. make sure the user enters an email
-        // 2. make sure the email address has at least one @ sign and one dot character
-
-        /*************************************************
-         * validate and process the phone number
-         ************************************************/
-        // 1. make sure the user enters at least seven digits, not including formatting characters
-        // 2. format the phone number like this 123-4567 or this 123-456-7890
-
-        /*************************************************
-         * Display the validation message
-         ************************************************/
+        if (empty($name)) {
+	    $message = 'Please enter your name.';
+	    break;
+	}
         $message ="Hello $name,\n\n" .
 	          "Thank you for entering this data:\n\n" .
 		  "Name: $name\n" .
